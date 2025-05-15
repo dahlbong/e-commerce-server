1834e861311a2caba41322e25ac719e46711cd52
dahyun <halover111@gmail.com>
2025-05-15 22:22:24 +0900

    [feat] 캐시 구현을 위한 템플릿 생성

--- src/main/java/kr/hhplus/be/server/support/cache/CacheTemplate.java
+++ src/main/java/kr/hhplus/be/server/support/cache/CacheTemplate.java
@@ -1,4 +1,12 @@
 package kr.hhplus.be.server.support.cache;
 
-public class CacheTemplate {
+import java.util.Optional;
+
+public interface CacheTemplate {
+
+    <T> Optional<T> get(Cacheable cacheable, String key, Class<T> type);
+
+    <T> void put(Cacheable cacheable, String key, T value);
+
+    void evict(Cacheable cacheable, String key);
 }
