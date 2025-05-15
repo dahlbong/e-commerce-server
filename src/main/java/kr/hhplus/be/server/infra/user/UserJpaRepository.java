93735daf257d92785fbda84946f793590bfabe90
dahyun <halover111@gmail.com>
2025-05-15 22:45:29 +0900

    [refactor] UserRepository 구현체 레이어 변경 및 생성

--- src/main/java/kr/hhplus/be/server/infra/user/UserJpaRepository.java
+++ src/main/java/kr/hhplus/be/server/infra/user/UserJpaRepository.java
@@ -3,7 +3,5 @@ package kr.hhplus.be.server.infra.user;
 import kr.hhplus.be.server.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface UserJpaRepository extends JpaRepository<User, Long> {
 }
