71987b409964c9dd7eed813d0a24c5181b6e0a91
dahyun <halover111@gmail.com>
2025-05-15 22:48:33 +0900

    [refactor] 인프라 레이어에서  payment와 order 분리

--- src/main/java/kr/hhplus/be/server/infra/payment/PaymentJpaRepository.java
+++ src/main/java/kr/hhplus/be/server/infra/payment/PaymentJpaRepository.java
@@ -1,4 +1,8 @@
 package kr.hhplus.be.server.infra.payment;
 
-public class PaymentJpaRepository {
+import kr.hhplus.be.server.domain.payment.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PaymentJpaRepository extends JpaRepository<Payment, Long> {
+
 }
