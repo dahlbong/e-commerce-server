6db1467674e37076fa7f81fd8fbddeb14471fb04
dahyun <halover111@gmail.com>
2025-05-21 22:09:23 +0900

    [test] Point 유닛테스트 / 통합테스트

---
+++ src/test/java/kr/hhplus/be/server/application/point/PointFacadeIntegrationTest.java
@@ -0,0 +1,68 @@
+package kr.hhplus.be.server.application.point;
+
+import kr.hhplus.be.server.domain.point.Point;
+import kr.hhplus.be.server.domain.point.PointRepository;
+import kr.hhplus.be.server.domain.user.User;
+import kr.hhplus.be.server.domain.user.UserRepository;
+import kr.hhplus.be.server.test.supporters.IntegrationTestSupport;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Transactional
+class PointFacadeIntegrationTest extends IntegrationTestSupport {
+
+    @Autowired
+    private PointFacade pointFacade;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PointRepository pointRepository;
+
+    private User user;
+
+    @BeforeEach
+    void setUp() {
+        user = User.create("항플");
+        userRepository.save(user);
+
+        Point point = Point.builder()
+            .userId(user.getId())
+            .amount(100_000L)
+            .build();
+        pointRepository.save(point);
+    }
+
+    @DisplayName("잔액을 충전한다.")
+    @Test
+    void chargePoint() {
+        // given
+        PointCriteria.Charge criteria = PointCriteria.Charge.of(user.getId(), 10_000L);
+
+        // when
+        pointFacade.chargePoint(criteria);
+
+        // then
+        Point point = pointRepository.findOptionalByUserId(user.getId()).orElseThrow();
+        assertThat(point.getAmount()).isEqualTo(110_000L);
+    }
+
+    @DisplayName("잔액을 조회한다.")
+    @Test
+    void getPoint() {
+        // given
+        Long userId = user.getId();
+
+        // when
+        PointResult.Point point = pointFacade.getPoint(userId);
+
+        // then
+        assertThat(point.getAmount()).isEqualTo(100_000L);
+    }
+}
---
+++ src/test/java/kr/hhplus/be/server/application/point/PointFacadeUnitTest.java
@@ -0,0 +1,60 @@
+package kr.hhplus.be.server.application.point;
+
+import kr.hhplus.be.server.domain.point.PointInfo;
+import kr.hhplus.be.server.domain.point.PointService;
+import kr.hhplus.be.server.domain.user.UserService;
+import kr.hhplus.be.server.test.supporters.MockTestSupport;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.InOrder;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
+
+class PointFacadeUnitTest extends MockTestSupport {
+
+    @InjectMocks
+    private PointFacade balanceFacade;
+
+    @Mock
+    private UserService userService;
+
+    @Mock
+    private PointService balanceService;
+
+    @DisplayName("잔액을 충전한다.")
+    @Test
+    void chargePoint() {
+        // given
+        PointCriteria.Charge criteria = mock(PointCriteria.Charge.class);
+
+        // when
+        balanceFacade.chargePoint(criteria);
+
+        // then
+        InOrder inOrder = inOrder(userService, balanceService);
+        inOrder.verify(userService, times(1)).getUser(criteria.getUserId());
+        inOrder.verify(balanceService, times(1)).chargePoint(criteria.toCommand());
+    }
+
+    @DisplayName("잔액을 조회한다.")
+    @Test
+    void getPoint() {
+        // given
+        Long userId = anyLong();
+
+        when(balanceService.getPoint(userId))
+            .thenReturn(PointInfo.Point.of(10_000L));
+
+        // when
+        PointResult.Point balance = balanceFacade.getPoint(userId);
+
+        // then
+        InOrder inOrder = inOrder(userService, balanceService);
+        inOrder.verify(userService, times(1)).getUser(userId);
+        inOrder.verify(balanceService, times(1)).getPoint(userId);
+        assertThat(balance.getAmount()).isEqualTo(10_000L);
+    }
+}
